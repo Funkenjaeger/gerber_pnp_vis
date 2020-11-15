9cae3b50994b89e4275dea9b0149892eab6fcef0
Funkenjaeger <evan.dudzik@gmail.com>
2020-11-15 14:30:09 -0500

    Initial commit

---
+++ gerber_render.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 08:29:18 2019
+
+@author: evand
+"""
+
+import sys
+import getopt
+import os
+from gerber import load_layer
+from gerber.render import RenderSettings, theme, GerberCairoContext
+from PIL import Image
+import configparser
+
+
+def isrendercurrent(BASE_FOLDER):
+    if not (os.path.exists(os.path.join(BASE_FOLDER, 'board_top.png')) and
+             os.path.exists(os.path.join(BASE_FOLDER, 'board_bottom.png')) and
+             os.path.exists(os.path.join(BASE_FOLDER, 'board.ini'))):
+        return False
+
+    config = configparser.ConfigParser()
+    config.read(os.path.join(BASE_FOLDER, 'board.ini'))
+
+    mtime = 0
+    for filename in os.listdir(os.path.join(BASE_FOLDER, 'GerberFiles')):
+        mtime = max(mtime, os.path.getmtime(os.path.join(BASE_FOLDER, 'GerberFiles', filename)))
+    ini_mtime = float(config['OTHER']['GERBERS_LASTMODIFIED'])
+    if mtime == ini_mtime:
+        return True
+    else:
+        return False
+
+
+def render(BASE_FOLDER):
+    ppi = 1000
+    # font_size_in = 0.02
+    # font_color = 'rgb(255, 0, 0)'
+    # textbox_color = "skyblue"
+    Image.MAX_IMAGE_PIXELS = 1000000000
+
+    if os.path.exists(os.path.join(BASE_FOLDER, 'board_top.png')):
+        os.remove(os.path.join(BASE_FOLDER, 'board_top.png'))
+    if os.path.exists(os.path.join(BASE_FOLDER, 'board_bottom.png')):
+        os.remove(os.path.join(BASE_FOLDER, 'board_bottom.png'))
+
+    print('Processing ', BASE_FOLDER)
+
+    # Open the gerber files
+    copper = load_layer(os.path.join(BASE_FOLDER, 'GerberFiles\copper_top.gbr'))
+    mask = load_layer(os.path.join(BASE_FOLDER, 'GerberFiles\soldermask_top.gbr'))
+    silk = load_layer(os.path.join(BASE_FOLDER, 'GerberFiles\silkscreen_top.gbr'))
+    outline = load_layer(os.path.join(BASE_FOLDER, 'GerberFiles\profile.gbr'))
+    drill = load_layer(os.path.join(BASE_FOLDER, 'DrillFiles\drill_1_16.xln'))
+
+    # Create a new drawing context
+    ctx = GerberCairoContext(scale=ppi)
+
+    # Draw the outline first, so it establishes the outer dimensions
+    ctx.render_layer(outline, settings=RenderSettings(color=theme.COLORS['black'], alpha=0.85))
+
+    # Draw the copper layer. render_layer() uses the default color scheme for the
+    # layer, based on the layer type. Copper layers are rendered as
+    ctx.render_layer(copper, settings=RenderSettings(color=theme.COLORS['hasl copper'], alpha=0.85))
+
+    # Draw the soldermask layer
+    ctx.render_layer(mask, settings=RenderSettings(color=theme.COLORS['green soldermask'], invert=True, alpha=0.8))
+
+    # Draw the silkscreen layer, and specify the rendering settings to use
+    ctx.render_layer(silk, settings=RenderSettings(color=theme.COLORS['white'], alpha=0.85))
+
+    # Draw the drill layer
+    ctx.render_layer(drill)
+
+    # Draw the outline again so it's on top of everything
+    ctx.render_layer(outline, settings=RenderSettings(color=theme.COLORS['black'], alpha=0.85))
+
+    size_px = ctx.size_in_pixels
+    origin_px = ctx.origin_in_pixels
+    origin_in = ctx.origin_in_inch
+    print(f"size: {ctx.size_in_inch} in / {ctx.size_in_pixels} px ({ppi} PPI)")
+    print(f"origin: {ctx.origin_in_inch} in / {ctx.origin_in_pixels} px")
+    config = configparser.ConfigParser()
+    config['DIMENSIONS'] = {'SIZE_X_IN': ctx.size_in_inch[0],
+                            'SIZE_Y_IN': ctx.size_in_inch[1],
+                            'SIZE_X_PX': ctx.size_in_pixels[0],
+                            'SIZE_Y_PX': ctx.size_in_pixels[1],
+                            'PPI': ppi,
+                            'ORIGIN_X_IN': ctx.origin_in_inch[0],
+                            'ORIGIN_Y_IN': ctx.origin_in_inch[1]}
+
+    mtime = 0
+    for filename in os.listdir(os.path.join(BASE_FOLDER, 'GerberFiles')):
+        mtime = max(mtime, os.path.getmtime(os.path.join(BASE_FOLDER, 'GerberFiles',filename)))
+    print('Latest file modification time: ', mtime)
+    config['OTHER'] = {'GERBERS_LASTMODIFIED': mtime}
+    with open(os.path.join(BASE_FOLDER,'board.ini'),'w') as configfile:
+        config.write(configfile)
+
+    # Write output to png file
+    ctx.dump(os.path.join(BASE_FOLDER, 'board_top.png'), verbose=True)
+
+    # Load the bottom layers
+    copper = load_layer(os.path.join(BASE_FOLDER, 'GerberFiles\copper_bottom.gbr'))
+    mask = load_layer(os.path.join(BASE_FOLDER, 'GerberFiles\soldermask_bottom.gbr'))
+    silk = load_layer(os.path.join(BASE_FOLDER, 'GerberFiles\silkscreen_bottom.gbr'))
+
+    # Clear the drawing
+    ctx.clear()
+
+    # Render bottom layers
+    ctx.render_layer(outline, settings=RenderSettings(color=theme.COLORS['black'], mirror=True, alpha=0.85))
+    ctx.render_layer(copper, settings=RenderSettings(color=theme.COLORS['hasl copper'], alpha=0.85))
+    ctx.render_layer(mask, settings=RenderSettings(color=theme.COLORS['green soldermask'], invert=True, alpha=0.8))
+    ctx.render_layer(silk, settings=RenderSettings(color=theme.COLORS['white'], alpha=0.85))
+    ctx.render_layer(drill)
+    ctx.render_layer(outline, settings=RenderSettings(color=theme.COLORS['black'], mirror=True, alpha=0.85))
+
+    # Write png file
+    ctx.dump(os.path.join(BASE_FOLDER, 'board_bottom.png'), verbose=True)
+
+
+if __name__ == "__main__":
+    BASE_FOLDER = './'
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:")
+    except getopt.GetoptError:
+        print('gerber_pnp_vis.py -i <inputdir>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-i":
+            BASE_FOLDER = arg
+    render(BASE_FOLDER)
---
+++ parse_assembly.py
@@ -0,0 +1,180 @@
+import pandas
+import sys
+import os
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QMainWindow, QTreeWidgetItem, QGraphicsScene, QGraphicsRectItem, QFileDialog, QGraphicsPixmapItem
+from PySide2.QtGui import QPixmap, QPen
+from PySide2.QtCore import QFile, Qt
+import gerber_render
+import configparser
+
+
+def populatebomtree():
+    global bom, assy_top, assy_bot, viewtop
+    bom = pandas.read_csv(os.path.join(BASE_FOLDER, 'Assembly\\bom.csv'), header=0)
+    assy_top = pandas.read_csv(os.path.join(BASE_FOLDER, 'Assembly\\PnP_front.csv'), header=0)
+    assy_bot = pandas.read_csv(os.path.join(BASE_FOLDER, 'Assembly\\PnP_back.csv'), header=0)
+
+    tw = window.treeWidget_bom
+    tw.clearSelection()
+    tw.clear()
+    assy_side = assy_top if viewtop else assy_bot
+    for index, row in bom.iterrows():
+        selected = str.split(row['Parts'], ', ')
+        pnp_refdes = assy_side['Name']
+        pnp_count = 0
+        parts_thisside = ''
+        for refdes in selected:
+            pnp_row = assy_side[pnp_refdes == refdes]
+            if len(pnp_row) > 0:
+                if pnp_count == 0:
+                    twi = QTreeWidgetItem(tw, [row['Parts'], row['Value'], row['Device'], row['Package'],
+                                               row['Description']])
+                parts_thisside = parts_thisside + ',' + refdes if pnp_count > 0 else refdes
+                pnp_count = pnp_count + 1
+                twi.addChild(QTreeWidgetItem(twi, [pnp_row['Name'].values[0]]))
+        if pnp_count > 0 and pnp_count < len(selected):
+            twi.setData(0, Qt.DisplayRole, parts_thisside)
+        if pnp_count == 1:
+            twi.removeChild(twi.child(0))
+
+
+class TestWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(TestWindow, self).__init__(parent)
+        loader = QUiLoader()
+        # file = QtCore.QFile(os.path.abspath("ui/mainwindow.ui"))
+        file = QFile("ui_gerber_pnp_vis.ui")
+        if file.open(QFile.ReadOnly):
+            self.window = loader.load(file, parent)
+            file.close()
+            self.setCentralWidget(self.window)
+            self.show()
+
+    def resizeEvent(self, event):
+        scaleboardimage()
+
+
+def scaleboardimage():
+    try:
+        if len(scene.items()) > 0:
+            view = window.graphicsView
+            scale = 0.99*min(view.width() / scene.width(), view.height() / scene.height())
+            window.graphicsView.setScene(scene)
+            window.graphicsView.resetTransform()
+            window.graphicsView.scale(scale, scale)
+    except NameError:
+        return
+
+
+def highlightparts():
+    for sceneItem in scene.items():
+        if type(sceneItem) not in [QGraphicsPixmapItem]:
+            scene.removeItem(sceneItem)
+    if len(window.treeWidget_bom.selectedItems()) > 0:
+        item = window.treeWidget_bom.selectedItems()[0]
+        if item.childCount() == 0:
+            highlightpart(item)
+        else:
+            for index in range(item.childCount()):
+                highlightpart(item.child(index))
+            window.graphicsView.setScene(scene)
+
+
+def highlightpart(item):
+    global origin_in, ppi, viewtop
+    rectsize = [25, 25]
+    assy_side = assy_top if viewtop else assy_bot
+    pnp_refdes = assy_side['Name']
+    refdes = item.data(0, 0)
+    pnp_row = assy_side[pnp_refdes == refdes]
+    if len(pnp_row) > 0:
+        x = (pnp_row['X']/25.4 - origin_in[0]) * ppi
+        if not viewtop:
+            x = scene.width() - x
+        y = scene.height() - (pnp_row['Y']/25.4 - origin_in[1]) * ppi
+        pen = QPen(Qt.yellow, 5, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin)
+        scene.addLine(x - rectsize[0], y - rectsize[1], x + rectsize[0], y + rectsize[1], pen)
+        scene.addLine(x - rectsize[0], y + rectsize[1], x + rectsize[0], y - rectsize[1], pen)
+        pen = QPen(Qt.red, 10, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin)
+        scene.addRect(x - rectsize[0] / 2, y - rectsize[1] / 2, rectsize[0], rectsize[1], pen)
+
+
+def loadboard():
+    global viewtop, pixmap_top, pixmap_bot, BASE_FOLDER, origin_in, ppi
+
+    dialog = QFileDialog()
+    # dialog.setFileMode(QFileDialog.Directory)
+    #dialog.setAcceptMode(QFileDialog.AcceptOpen)
+    BASE_FOLDER = dialog.getExistingDirectory(window, "Select CAMOutputs directory to process...", '.', QFileDialog.ShowDirsOnly)
+
+    window.statusbar.showMessage('Processing ' + BASE_FOLDER)
+    window.statusbar.repaint()
+    if gerber_render.isrendercurrent(BASE_FOLDER):
+        print('Render is current')
+    else:
+        gerber_render.render(BASE_FOLDER)
+
+    config = configparser.ConfigParser()
+    config.read(os.path.join(BASE_FOLDER, 'board.ini'))
+    origin_in = [float(config['DIMENSIONS']['origin_x_in']), float(config['DIMENSIONS']['origin_y_in'])]
+    ppi = float(config['DIMENSIONS']['ppi'])
+
+    pixmap_top = QPixmap(os.path.join(BASE_FOLDER, 'board_top.png'))
+    pixmap_bot = QPixmap(os.path.join(BASE_FOLDER, 'board_bottom.png'))
+    setboardtop()
+
+    window.actionSideTop.setChecked(True)
+    window.actionSideBottom.setChecked(False)
+    window.statusbar.showMessage('Done processing board', 10000)
+
+
+def setboardtop():
+    global viewtop
+    viewtop = True
+    window.actionSideTop.setChecked(True)
+    window.actionSideBottom.setChecked(False)
+    setboardside()
+
+
+def setboardbot():
+    global viewtop
+    viewtop = False
+    window.actionSideTop.setChecked(False)
+    window.actionSideBottom.setChecked(True)
+    setboardside()
+
+
+def setboardside():
+    global viewtop
+    try:
+        scene.clear()
+        if viewtop:
+            scene.addPixmap(pixmap_top)
+        else:
+            scene.addPixmap(pixmap_bot)
+        scaleboardimage()
+        populatebomtree()
+    except NameError:
+        window.actionSideTop.setChecked(False)
+        window.actionSideBottom.setChecked(False)
+        window.statusbar.showMessage('No board loaded', 3000)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    BASE_FOLDER = '.'
+    viewtop = True
+
+    test = TestWindow()
+    window = test.window
+    scene = QGraphicsScene()
+    window.splitter.splitterMoved.connect(scaleboardimage)
+    window.treeWidget_bom.itemSelectionChanged.connect(highlightparts)
+    window.action_processgerbers.triggered.connect(loadboard)
+    window.actionSideTop.triggered.connect(setboardtop)
+    window.actionSideBottom.triggered.connect(setboardbot)
+
+    window.statusbar.showMessage('No board loaded')
+
+    sys.exit(app.exec_())
